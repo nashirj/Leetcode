2713d9b34fa8ac3bb650bc6ce43370190c4662f5
Nashir Janmohamed <nashirbass@gmail.com>
2019-02-02 21:59:22 -0500

    prepping for interview

--- 204-Count-Primes.cpp
+++ 204-Count-Primes.cpp
@@ -1,4 +1,6 @@
-/*Count Primes
+/*
+204. Count Primes
+
 Count the number of prime numbers less than a non-negative number, n.
 
 Example:
@@ -7,52 +9,215 @@ Input: 10
 Output: 4
 Explanation: There are 4 prime numbers less than 10, they are 2, 3, 5, 7.
 */
+
+
 class Solution {
 public:
-    static int primesLessThan100[] = {7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97};
-    bool isPrime(int num){
-        if (num % 2 == 0 || num % 5 == 0)
-            return false;
-        int divBy3 = 0;
-        while(num >= 0){
-            divBy3 += num%10;
-            num /= 10;
+    int countPrimes(int n) {
+        if (n < 3)
+            return 0;
+        
+        bool isPrime[n];
+        memset(isPrime, true, sizeof(isPrime));
+        isPrime[0] = isPrime[1] = false;
+        
+        int numPrimes = 0;
+        
+        long long p = 2;
+        while(p * p < n){
+            if (isPrime[p]){
+                for (long long i = p * p; (i < n) && (i >= 0); i += p){
+                    if (i < 0) break;
+                    isPrime[i] = false;
+                }
+                numPrimes++;
+            }
+            p++;
         }
-        if(divBy3 % 3 == 0)
-            return false;
-        double result = sqrt(double(num));
-        int res = int(result);
-        cout << "res is " << res << endl;
-        for(int i : primesLessThan100){
-            if (i > res)
-                break;
-            if (num % i == 0)
-                return false;
+        
+        while (p < n){
+            if (isPrime[p])
+                numPrimes++;
+            p++;
         }
-        return true;
+            
+        
+        return numPrimes;
+        
     }
-    
+};
+
+
+
+class Solution {
+public:
+    int countPrimes(int n) {
+        if (n < 3)
+            return 0;
+        
+        bool isPrime[n];
+        memset(isPrime, true, sizeof(isPrime));
+        isPrime[0] = isPrime[1] = false;
+        
+        int numPrimes = 0;
+        
+        for (long long p = 2; p < n; p++){
+            if (isPrime[p]){
+                for (long long i = p * p; (i < n) && (i >= 0); i += p){
+                    if (i < 0) break;
+                    isPrime[i] = false;
+                }
+                
+                numPrimes++;
+            }
+        }
+        
+        return numPrimes;
+        
+    }
+};
+
+
+class Solution {
+public:
+    int countPrimes(int n) {
+        if (n < 3)
+            return 0;
+        
+        vector<bool> isPrime(n, true);
+        isPrime[0] = isPrime[1] = false;
+        
+        int numPrimes = 0;
+        
+        for (long long p = 2; p < n; p++){
+            if (isPrime[p]){
+                for (long long i = p * p; (i < n) && (i >= 0); i += p){
+                    if (i < 0) break;
+                    isPrime[i] = false;
+                }
+                
+                numPrimes++;
+            }
+        }
+        
+        return numPrimes;
+        
+    }
+};
+
+
+class Solution {
+public:
+    int countPrimes(int n) {
+        if (n < 3)
+            return 0;
+        
+        vector<bool> isPrime(n, true);
+        int numPrimes = 0, p = 2;
+        
+        while (p * p < n){
+            if (isPrime[p]){
+                for (int i = p * p; i < n; i += p){
+                    isPrime[i] = false;
+                }
+                numPrimes++;
+            }
+            p++;
+        }
+        
+        while (p < n){
+            if (isPrime[p])
+                numPrimes++;
+            p++;
+        }
+        
+        return numPrimes;        
+    }
+};
+
+class Solution {
+public:
     int countPrimes(int n) {
         if (n <= 2)
             return 0;
-        if (n == 3)
-            return 1;
-        int numOfPrimes = 2;
-        for (int i = 1; i < n; i++){ //will never get close to n. ok to reduce this way?
-            int low = (6 * i) - 1;
-            int high = low + 2;
-            cout << low << endl << high << endl;
-            if (low < n){
-                if(isPrime(low))
-                    numOfPrimes++;
+        
+        vector<bool> isPrime(n, true);
+        
+        for (int p = 2; p * p < n; p++){
+            for (int i = p * p; i < n; i += p){
+                isPrime[i] = false;
             }
-            if (high < n){
-                if(isPrime(high))
-                    numOfPrimes++;
-            }
-            if (low >= n || high >= n)
-                break;
         }
-        return numOfPrimes;
+        
+        int numPrimes = 0;
+        
+        for (int i = 2; i < n; i++){
+            if (isPrime[i])
+                numPrimes++;
+        }
+        
+        return numPrimes;
+        
+    }
+};
+
+class Solution {
+public:
+    int countPrimes(int n) {
+        if (n <= 2)
+            return 0;
+        
+        vector<bool> isPrime(n, true);
+        isPrime[0] = isPrime[1] = false;
+        
+        for (int p = 2; p * p < n; p++){
+            for (int i = p * 2; i < n; i += p){
+                isPrime[i] = false;
+            }
+        }
+        
+        int numPrimes = 0;
+        
+        for (bool val : isPrime){
+            if (val)
+                numPrimes++;
+        }
+        
+        return numPrimes;
+        
+    }
+};
+
+//1st solution using set
+
+class Solution {
+public:
+    int countPrimes(int n) {
+        unordered_set<int> notPrime;
+        
+        if (n <= 2)
+            return 0;
+        
+        int curr = 2, numPrimes = 1;
+        for (int i = curr; i < n; i += 2){
+            notPrime.insert(i);
+        }
+        curr++;
+        while (curr < n){
+            if (notPrime.find(curr) == notPrime.end())
+                numPrimes++;
+            
+            if (curr * curr >= n){
+                curr++;
+                continue;
+            }
+              
+            for (int i = curr * 2; i < n; i += curr){
+                notPrime.insert(i);
+            }
+            curr++;
+        }
+        return numPrimes;
+        
     }
 };
