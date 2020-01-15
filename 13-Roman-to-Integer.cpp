2af714761f6b6d3fa4d9fd59a6ce9b53002c9965
Nashir Janmohamed <nashirbass@gmail.com>
2020-01-15 14:03:14 +0300

    did some leetcode problems

--- 13-Roman-to-Integer.cpp
+++ 13-Roman-to-Integer.cpp
@@ -44,6 +44,46 @@ Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
 */
 
 
+// SOLUTION FROM 1/15/2020
+class Solution {
+public:
+    int romanToInt(string s) {
+        int n = s.length();
+        int total = 0;
+        // if s[i] < s[i+1], then process both at once, else process one at a time
+        for (int i = 0; i < n; i++) {
+            if (i < n-1 && get_conv(s[i]) < get_conv(s[i+1])) {
+                total -= get_conv(s[i]);
+                i++;
+            }
+            total += get_conv(s[i]);   
+        }
+        return total;
+    }
+    int get_conv(char c) {
+        switch(c) {
+            case 'I':
+                return 1;
+            case 'V':
+                return 5;
+            case 'X':
+                return 10;
+            case 'L':
+                return 50;
+            case 'C':
+                return 100;
+            case 'D':
+                return 500;
+            case 'M':
+                return 1000;
+            default:
+                return 0;
+        }
+    }
+};
+
+
+// SOLUTION FROM BEFORE
 class Solution {
 public:
     int romanToInt(string s) {
