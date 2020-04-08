636aba01779a23bb5df39562be23ec8c336d0e44
Nashir Janmohamed <nashirbass@gmail.com>
2020-04-08 09:43:01 -0700

    Add new solution for middle of the linked list problem

--- leetcode/876-Middle-of-the-Linked-List.cpp
+++ leetcode/876-Middle-of-the-Linked-List.cpp
@@ -22,6 +22,40 @@ Note:
 The number of nodes in the given list will be between 1 and 100.
 */
 
+// 1 pass algorithm
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        if (!head) {
+            return nullptr;
+        }
+        
+        ListNode* slow = head;
+        ListNode* fast = head->next;
+        while (fast) {
+            slow = slow->next;
+            fast = fast->next;
+            if (!fast) {
+                break;
+            }
+            fast = fast->next;
+        }
+        return slow;
+    }
+};
+
+
+// 2 pass algorithm
+
 /**
  * Definition for singly-linked list.
  * struct ListNode {
