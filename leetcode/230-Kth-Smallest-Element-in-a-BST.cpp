3053c57bda9135c0ca6584982b09a180065b8bf8
Nashir Janmohamed <nashirbass@gmail.com>
2020-05-20 17:47:58 -0700

    Daily leetcode

--- leetcode/230-Kth-Smallest-Element-in-a-BST.cpp
+++ leetcode/230-Kth-Smallest-Element-in-a-BST.cpp
@@ -30,6 +30,44 @@ Follow up:
 What if the BST is modified (insert/delete operations) often and you need to find the kth smallest frequently? How would you optimize the kthSmallest routine?
 */
 
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int kthSmallest(TreeNode* root, int k) {
+        int count = 0;
+        return helper(root, k, count);
+    }
+    int helper(TreeNode* root, int k, int& count) {
+        if (!root) {
+            return INT_MIN;
+        }
+        int left = helper(root->left, k, count);
+        if (left != INT_MIN) {
+            return left;
+        }
+        
+        count++;
+        if (count == k) {
+            return root->val;
+        }
+        
+        return helper(root->right, k, count);
+    }
+};
+
+
+
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
