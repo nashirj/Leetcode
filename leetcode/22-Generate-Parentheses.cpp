22a952fb0918d2f73f6a9a3dad1f3ad634d6f9ea
Nashir Janmohamed <nashirbass@gmail.com>
2020-07-26 12:38:35 -0400

    Add files via upload

---
+++ leetcode/22-Generate-Parentheses.cpp
@@ -0,0 +1,36 @@
+/*
+22. Generate Parentheses
+Medium
+
+Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+
+For example, given n = 3, a solution set is:
+
+[
+  "((()))",
+  "(()())",
+  "(())()",
+  "()(())",
+  "()()()"
+]
+*/
+
+class Solution {
+public:
+    vector<string> generateParenthesis(int n) {
+        vector<string> res = vector<string>();
+        backtrack(res, "", 0, 0, n);
+        return res;
+    }
+    void backtrack(vector<string>& res, string s, int open, int close, int n) {
+        if (s.size() == n * 2) {
+            res.push_back(s);
+        }
+        if (close < open) {
+            backtrack(res, s + ')', open, close+1, n);
+        }
+        if (open < n) {
+            backtrack(res, s + '(', open+1, close, n);
+        }
+    }
+};
